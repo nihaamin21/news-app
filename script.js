578392b038e2759eac52461252a0f316438ddaf0
nihaamin21 <nihaamin21@gmail.com>
2018-08-26 05:38:27 +0500

    some changes

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 $.ajax({
     url: "https://newsapi.org/v2/top-headlines?sources=nbc-news&apiKey=70de3c70d23e4af78098dcb1c5f0ab5b" ,
     success: function(data)
-    {
-        for(i=0; i<data.article8 ; i++){
-            document.querySelector(".row").innerHTML += `
-            <div class="column">
-                <div class="content">
-                    <img class="img-1" src='${data.articles[i].urlToImage}'>
-                    <h3>${data.articles[i].title}</h3>
-                    <p>${data.articles[i].description}</p>
-                </div>
-            </div>`
+    {  
+            document.querySelector('.img-1').style.background-image = `url(${data.articles[0].urlToImage});`
+            document.querySelector('h3').innerHTML = `${data.articles[0].title}`;
+            document.querySelector('p').innerHTML = `${data.articles[0].description}`;
         }
     },
     eror:function(error){
