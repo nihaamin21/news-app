af9734c09bc23036bed904a2250bd38c2959c3e7
niha amin <38943103+nihaamin21@users.noreply.github.com>
2018-09-04 17:58:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $.ajax({
     url: "https://newsapi.org/v2/top-headlines?sources=nbc-news&apiKey=70de3c70d23e4af78098dcb1c5f0ab5b" ,
     success: function(data)
     {  
-            document.querySelector('.img-1').style.background-image = `url(${data.articles[0].urlToImage});`
+            document.querySelector('.img-1').style["background-image"] = `url(${data.articles[0].urlToImage});`
             document.querySelector('h3').innerHTML = `${data.articles[0].title}`;
             document.querySelector('p').innerHTML = `${data.articles[0].description}`;
-        }
+        
     },
-    eror:function(error){
+    error :function(error){
         alert(error.response.JSON.message);
     }
 });
