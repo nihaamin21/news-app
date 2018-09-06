386047d548f2b77109732651bebae18af9e4324c
VB Student <vbstudent@uit.edu>
2018-09-06 13:42:47 +0500

    commit by me

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $.ajax({
     url: "https://newsapi.org/v2/top-headlines?sources=nbc-news&apiKey=70de3c70d23e4af78098dcb1c5f0ab5b" ,
     success: function(data)
     {  
-            document.querySelector('.img-1').style["background-image"] = `url(${data.articles[0].urlToImage});`
-            document.querySelector('h3').innerHTML = `${data.articles[0].title}`;
-            document.querySelector('p').innerHTML = `${data.articles[0].description}`;
+            document.querySelector('#img-1').style["background-image"] = `url(${data.articles[1].urlToImage})`;
+            document.querySelector('#title-1').innerHTML = `${data.articles[1].title}`;
+            document.querySelector('#discription-1').innerHTML = `${data.articles[1].description}`;
         
     },
     error :function(error){
